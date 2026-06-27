748568787042edbafb23ff26550eaca6c46cae78
levong1803 <levong1803@users.noreply.github.com>
2026-06-28 01:09:17 +0700

    fix: change default proxy port to 8000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {
-        target: `http://127.0.0.1:${process.env.VITE_API_PORT || "8001"}`,
+        target: `http://127.0.0.1:${process.env.VITE_API_PORT || "8000"}`,
         changeOrigin: true,
       },
     },
